c76c7351593f7fd21c8d023ecf5eca1400dc00dd
John McDole <jtmcdole@gmail.com>
2015-11-07 08:22:39 -0800

    Rename to _redraw() to what it really is doing.. select the graphics

--- src/options.c
+++ src/options.c
@@ -1216,7 +1216,7 @@ bestGraphics()
 
 /* We need to select this after all the options have been parsed - given that
  * parseoptions() is called recusively, this would harm other options */
-void _redraw() {
+void _select_graphics() {
 # ifdef REINCARNATION
     if (!initial && Is_rogue_level(&u.uz)) assign_rogue_graphics(FALSE);
 # endif
@@ -2832,7 +2832,7 @@ doset()
 
 	destroy_nhwindow(tmpwin);
 	if (need_redraw) {
-        _redraw();
+        _select_graphics();
 	    (void) doredraw();
 	}
 	return 0;
